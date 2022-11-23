4de70573008ff9b40e6701b79cd2cb4074e358de
Anny023 <89271551+Anny023@users.noreply.github.com>
2022-11-23 13:04:05 +0530

    spotify clone added

---
+++ spotify-clone-blockchain/utils/const.js
@@ -0,0 +1,10 @@
+import { clusterApiUrl, PublicKey } from '@solana/web3.js'
+import spotify from './spotify.json'
+
+export const SOLANA_HOST = 'https://muddy-shy-dawn.solana-devnet.quiknode.pro/5b638e723dd3df716748cb5432458a2e21e53e05/'
+
+export const STABLE_POOL_PROGRAM_ID = new PublicKey(
+  '6WVjqUes7U4sU92t3myjLZRiSp7p2rL1XSt4JUK7Qp8z',
+)
+
+export const STABLE_POOL_IDL = spotify
